ce3cdaa6eb49e45cb068ecb827e189e54bd257c2
azismmz <mochamad.mmz@bsi.ac.id>
2024-03-12 13:06:50 +0700

    Update URL v2

--- app/Http/Controllers/Api/Mhs/LoginmhsController.php
+++ app/Http/Controllers/Api/Mhs/LoginmhsController.php
@@ -31,7 +31,7 @@ class LoginmhsController extends Controller
         }
 
         // URL sistem ujian online
-        $examSystemUrl = 'https://devujian/authenticate';
+        $examSystemUrl = 'https://devujian.bsi.ac.id/authenticate';
 
         // Redirect ke sistem ujian online dengan token sebagai parameter
         return redirect()->away("{$examSystemUrl}?token={$token}");

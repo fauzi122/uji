7df91f4002402dc222396feab3053b979ce509de
azismmz <mochamad.mmz@bsi.ac.id>
2024-03-13 11:42:49 +0700

    update read link youtube

--- app/Http/Middleware/AddCspHeaders.php
+++ app/Http/Middleware/AddCspHeaders.php
@@ -37,12 +37,14 @@ class AddCspHeaders
         $response->headers->set(
             'Content-Security-Policy',
             "default-src 'self';" .
-            "script-src 'self' 'unsafe-inline' 'unsafe-eval' example.com https://code.jquery.com https://cdnjs.cloudflare.com;" . // 'unsafe-eval' added here
-            "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://code.ionicframework.com https://cdnjs.cloudflare.com;" . // cdnjs.cloudflare.com already included
-            "font-src 'self' https://fonts.gstatic.com;" .
-            "connect-src 'self';"
+                "script-src 'self' 'unsafe-inline' 'unsafe-eval' example.com https://code.jquery.com https://cdnjs.cloudflare.com;" . // 'unsafe-eval' added here
+                "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://code.ionicframework.com https://cdnjs.cloudflare.com;" . // cdnjs.cloudflare.com already included
+                "font-src 'self' https://fonts.gstatic.com;" .
+                "img-src 'self' https://ytimg.com;" . // Added for YouTube images
+                "frame-src 'self' https://www.youtube.com https://youtube.com;" . // Added for YouTube iframes
+                "connect-src 'self';"
         );
-        
+
 
 
         return $response;

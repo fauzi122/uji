b3872f223917d4205a79374e781cafb4b642da6f
azismmz <mochamad.mmz@bsi.ac.id>
2024-03-13 15:47:32 +0700

    Perubahan Konflik

--- app/Http/Middleware/AddCspHeaders.php
+++ app/Http/Middleware/AddCspHeaders.php
@@ -40,8 +40,6 @@ class AddCspHeaders
                 "script-src 'self' 'unsafe-inline' 'unsafe-eval' example.com https://code.jquery.com https://cdnjs.cloudflare.com;" . // 'unsafe-eval' added here
                 "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://code.ionicframework.com https://cdnjs.cloudflare.com;" . // cdnjs.cloudflare.com already included
                 "font-src 'self' https://fonts.gstatic.com;" .
-                "img-src 'self' https://ytimg.com;" . //outube
-                "frame-src 'self' https://www.youtube.com https://youtube.com;" . // Added for YouTube iframes
                 "connect-src 'self';"
         );
 

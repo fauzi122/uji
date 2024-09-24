70d7e5696f986441778f32746b2deb4c5b8c2667
azismmz <mochamad.mmz@bsi.ac.id>
2024-09-24 08:17:27 +0700

    Insert Redis

--- app/Http/Controllers/Dosen/JadwalkuliahController.php
+++ app/Http/Controllers/Dosen/JadwalkuliahController.php
@@ -9,37 +9,42 @@ use Carbon\Carbon;
 use Illuminate\Database\QueryException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class JadwalkuliahController extends Controller
 {
-
-
   function index($id)
   {
-    // dd("asd");
     $sekarang = Carbon::now();
     $namaHari = $sekarang->locale('id')->dayName;
     $datasays = SaysSetting::where('status_setting', 'aktif')->orderBy('updated_at', 'desc')->firstOrFail();
     $ref_cabang = Ref_cabang::where('kd_kampus', $id)->firstOrFail();
-    $ref_cabangpilih = Ref_cabang::select('kd_kampus', 'nm_kampus')->whereNotIn('kd_kampus', ['I2', 'I1', 'X1', 'N', 'B5', 'K3', 'B3', 'A7', 'K8', 'A5', 'K2'])->orderby('nm_kampus', 'asc')->get();
+    $ref_cabangpilih = Ref_cabang::select('kd_kampus', 'nm_kampus')
+      ->whereNotIn('kd_kampus', ['I2', 'I1', 'X1', 'N', 'B5', 'K3', 'B3', 'A7', 'K8', 'A5', 'K2'])
+      ->orderby('nm_kampus', 'asc')
+      ->get();
 
     try {
-      $datajadwal = DB::table('jadwal')
-        ->select(
-          'jam_t',
-          'no_ruang',
-          DB::raw("SUBSTRING_INDEX(no_ruang, '-', 1) as no_ruangx"),
-          DB::raw("SUBSTRING_INDEX(kd_lokal, '-', 1) as kd_lokalx"), // Perbaikan di sini
-          'kel_praktek',
-          'nm_mtk',
-          'kd_dosen',
-          DB::raw("UPPER(nm_dosen) as nm_dosen")
-        )
-        ->where(DB::raw('RIGHT(no_ruang, 2)'), '=', $id)
-        ->where('hari_t', $namaHari)
+      // Cek apakah data jadwal sudah ada di Redis
+      $cacheKey = "jadwal_kampus_$id";
+      $datajadwal = Cache::remember($cacheKey, 60 * 60 * 24, function () use ($id, $namaHari) {
+        // return DB::table('jadwal')
+        //   ->select(
+        //     'jam_t',
+        //     'no_ruang',
+        //     DB::raw("SUBSTRING_INDEX(no_ruang, '-', 1) as no_ruangx"),
+        //     DB::raw("SUBSTRING_INDEX(kd_lokal, '-', 1) as kd_lokalx"), // Perbaikan di sini
+        //     'kel_praktek',
+        //     'nm_mtk',
+        //     'kd_dosen',
+        //     DB::raw("UPPER(nm_dosen) as nm_dosen")
+        //   )
+        //   ->where(DB::raw('RIGHT(no_ruang, 2)'), '=', $id)
+        //   ->where('hari_t', $namaHari)
+        //   ->orderByRaw('SUBSTRING(jam_t, 1, LOCATE("-", jam_t) - 1)')
+        //   ->get();
+      });
 
-        ->orderByRaw('SUBSTRING(jam_t, 1, LOCATE("-", jam_t) - 1)')
-        ->get();
       // Konversi datajadwalharian ke JSON
       $datajadwalharianJson = $datajadwal->toJson();
 

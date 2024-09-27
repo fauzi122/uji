0a99ea5e2bf281e3015874d7fd7e8451d080f8e3
azismmz <mochamad.mmz@bsi.ac.id>
2024-09-27 15:32:43 +0700

    Add Assets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,20 +5,24 @@
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  {{--  <title>Arsha Bootstrap Template - Index</title>  --}}
-        <script language='JavaScript'>
-            var txt=" My Best | Universitas Bina Sarana Informatika |";
-            var speed=300;
-            var refresh=null;
-            function action() { document.title=txt;
-            txt=txt.substring(1,txt.length)+txt.charAt(0);
-            refresh=setTimeout("action()",speed);}action();
-        </script>
+  {{-- <title>Arsha Bootstrap Template - Index</title>  --}}
+  <script language='JavaScript'>
+    var txt = " My Best | Universitas Bina Sarana Informatika |";
+    var speed = 300;
+    var refresh = null;
+
+    function action() {
+      document.title = txt;
+      txt = txt.substring(1, txt.length) + txt.charAt(0);
+      refresh = setTimeout("action()", speed);
+    }
+    action();
+  </script>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-	<link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}">
   <link href="{{ asset('assets/img/icon1.jfif') }}" rel="icon">
   <link href="{{ Storage::url('public/logo/ubsi.png') }}" rel="apple-touch-icon">
 
@@ -44,7 +48,7 @@
   <header id="header" class="fixed-top ">
     <div class="container d-flex align-items-center">
 
-      {{-- <h1 class="logo me-auto"><a href="/"><img src="{{ Storage::url('public/mybest_3.png') }}" alt="" loading="lazy"  class="img-fluid" width="150" height="200"></a></h1> --}}
+      {{-- <h1 class="logo me-auto"><a href="/"><img src="{{ Storage::url('public/mybest_3.png') }}" alt="" loading="lazy" class="img-fluid" width="150" height="200"></a></h1> --}}
       <!-- Uncomment below if you prefer to use an image logo -->
       <!-- <a href="index.html" class="logo me-auto"><img src="{{ asset('home/assets/img/logo.png')}}" alt="" loading="lazy"  class="img-fluid"></a>-->
 
@@ -70,56 +74,56 @@
           <div class="d-flex justify-content-center justify-content-lg-start">
             @if (Route::has('login'))
             <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                @auth
-                    @if (auth()->user()->utype === 'MHS')
-                        <a href="{{ url('/user/dashboard') }}" class="btn-get-started scrollto">Dashboard Mahasiswa</a>
-                    @elseif (auth()->user()->utype === 'ADM')
-                        <a href="{{ url('/dashboard') }}" class="btn-get-started scrollto">Dashboard Admin</a>
-                    @endif
-                @else
-                    <a href="{{ route('login') }}" class="btn-get-started scrollto">Masuk</a>
+              @auth
+              @if (auth()->user()->utype === 'MHS')
+              <a href="{{ url('/user/dashboard') }}" class="btn-get-started scrollto">Dashboard Mahasiswa</a>
+              @elseif (auth()->user()->utype === 'ADM')
+              <a href="{{ url('/dashboard') }}" class="btn-get-started scrollto">Dashboard Admin</a>
+              @endif
+              @else
+              <a href="{{ route('login') }}" class="btn-get-started scrollto">Masuk</a>
 
-                  
-                @endauth
+
+              @endauth
             </div>
-        @endif      
+            @endif
           </div>
         </div>
         <div class="col-lg-6 order-1 order-lg-2 hero-img" data-aos="zoom-in" data-aos-delay="200">
-          <img src="{{ asset('assets/assets_dasboard/img/home.svg')}}" class="img-fluid animated" alt="" loading="lazy" >
+          <img src="{{ asset('assets/assets_dasboard/img/home.svg')}}" class="img-fluid animated" alt="" loading="lazy">
         </div>
       </div>
     </div>
 
   </section><!-- End Hero -->
-  
+
   <main id="main">
     <!-- ======= Clients Section ======= -->
     <section id="clients" class="clients section-bg">
       <div class="container">
-        <div class="row" >
+        <div class="row">
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="{{ Storage::url('public/logo/ubsi_t.png') }}" class="img-fluid" alt="" loading="lazy" >
+            <!-- <img src="{{ Storage::url('public/logo/ubsi_t.png') }}" class="img-fluid" alt="" loading="lazy" > -->
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="{{ Storage::url('public/logo/logo_campus.png') }}" class="img-fluid" alt="" loading="lazy" >
+            <!-- <img src="{{ Storage::url('public/logo/logo_campus.png') }}" class="img-fluid" alt="" loading="lazy" > -->
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="{{ Storage::url('public/logo/logo_alumni.png') }}" class="img-fluid" alt="" loading="lazy" >
+            <!-- <img src="{{ Storage::url('public/logo/logo_alumni.png') }}" class="img-fluid" alt="" loading="lazy" > -->
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="{{ Storage::url('public/logo/lldikti3.jpg') }}" class="img-fluid" alt="" loading="lazy" >
+            <!-- <img src="{{ Storage::url('public/logo/lldikti3.jpg') }}" class="img-fluid" alt="" loading="lazy" > -->
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="{{ Storage::url('public/logo/kampus_merdeka.jpg') }}" class="img-fluid" alt="" loading="lazy" >
+            <!-- <img src="{{ Storage::url('public/logo/kampus_merdeka.jpg') }}" class="img-fluid" alt="" loading="lazy" > -->
           </div>
 
           <div class="col-lg-2 col-md-4 col-6 d-flex align-items-center justify-content-center">
-            <img src="{{ Storage::url('public/logo/icei.png') }}" class="img-fluid" alt="" loading="lazy" >
+            <!-- <img src="{{ Storage::url('public/logo/icei.png') }}" class="img-fluid" alt="" loading="lazy" > -->
           </div>
 
         </div>
@@ -134,7 +138,7 @@
 
         <div class="row">
           <div class="col-lg-6 d-flex align-items-center" data-aos="fade-right" data-aos-delay="100">
-            <img src="{{ asset('assets/img/about-img.png')}}" class="img-fluid" alt="" loading="lazy" >
+            <img src="{{ asset('assets/img/about-img.png')}}" class="img-fluid" alt="" loading="lazy">
           </div>
           <div class="col-lg-6 pt-4 pt-lg-0 content" data-aos="fade-left" data-aos-delay="100">
             <h3>Fitur Aplikasi MyBest</h3>
@@ -195,7 +199,7 @@
     </section><!-- End Skills Section -->
 
     <!-- ======= Services Section ======= -->
-    {{--  <section id="services" class="services section-bg">
+    {{-- <section id="services" class="services section-bg">
       <div class="container" data-aos="fade-up">
 
         <div class="section-title">
@@ -265,7 +269,7 @@
 
         <div class="section-title">
           <h2>Contact</h2>
-          {{--  <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>  --}}
+          {{-- <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>  --}}
         </div>
 
         <div class="row">
@@ -289,19 +293,19 @@
                 <h4>Call:</h4>
                 <p>(021)8000063</p>
               </div>
-              
-                <div class="phone">
+
+              <div class="phone">
                 <i class="bi bi-phone"></i>
                 <h4>Hotline 1:</h4>
                 <p>081381507561</p>
               </div>
-                 <div class="phone">
+              <div class="phone">
                 <i class="bi bi-phone"></i>
                 <h4>Hotline 2:</h4>
                 <p>081381507125</p>
               </div>
 
-              {{--  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.6515850141262!2d106.84221341517201!3d-6.183408590112826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5346b7c250b%3A0x189a490ffddab322!2sUBSI%20Kramat%2098!5e0!3m2!1sid!2sid!4v1582606989163!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>  --}}
+              {{-- <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d991.6515850141262!2d106.84221341517201!3d-6.183408590112826!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f5346b7c250b%3A0x189a490ffddab322!2sUBSI%20Kramat%2098!5e0!3m2!1sid!2sid!4v1582606989163!5m2!1sid!2sid" frameborder="0" style="border:0; width: 100%; height: 290px;" allowfullscreen></iframe>  --}}
             </div>
 
           </div>
@@ -327,7 +331,7 @@
                 <textarea class="form-control" name="message" rows="10" required></textarea>
               </div>
               <div class="my-3">
-                {{--  <div class="loading">Loading</div>  --}}
+                {{-- <div class="loading">Loading</div>  --}}
                 <div class="error-message"></div>
                 <div class="sent-message">Your message has been sent. Thank you!</div>
               </div>
@@ -348,7 +352,7 @@
     <div class="footer-newsletter">
       <div class="container">
         <div class="row justify-content-center">
-          {{--  <div class="col-lg-6">
+          {{-- <div class="col-lg-6">
             <h4>Join Our Newsletter</h4>
             <p>Tamen quem nulla quae legam multos aute sint culpa legam noster magna</p>
             <form action="" method="post">
@@ -364,7 +368,7 @@
         <div class="row">
 
           <div class="col-lg-3 col-md-6 footer-contact">
-            <h3><img src="{{ Storage::url('public/mybest_3.png') }}" alt="" loading="lazy"  class="img-fluid" width="150" height="100"></h3>
+            <h3><img src="{{ Storage::url('public/mybest_3.png') }}" alt="" loading="lazy" class="img-fluid" width="150" height="100"></h3>
             <p>
               Jl. Kramat Raya No.98, RW.9, Kwitang, Kec. Senen, Kota Jakarta Pusat, Daerah Khusus Ibukota Jakarta 10450<br><br>
               <strong>Phone:</strong> (021) 8000063<br>
@@ -425,72 +429,72 @@
 
   <div id="preloader"></div>
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
-<!-- Button trigger modal -->
+  <!-- Button trigger modal -->
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Pendaftaran MBKM</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <form>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Email</label>
-            <input type="text" class="form-control" placeholder="example@gmail.com">
-          </div>
-          {{--  <div class="form-group">
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLongTitle">Pendaftaran MBKM</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <form>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Email</label>
+              <input type="text" class="form-control" placeholder="example@gmail.com">
+            </div>
+            {{-- <div class="form-group">
             <label for="recipient-name" class="col-form-label">Nama Pengguna</label>
             <input type="text" class="form-control" placeholder="Nama Pengguna">
           </div>  --}}
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">NIM</label>
-            <input type="text" class="form-control" placeholder="NIM">
-          </div>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Nama Depan</label>
-            <input type="text" class="form-control" placeholder="Nama Depan">
-          </div>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Nama Belakang</label>
-            <input type="text" class="form-control" placeholder="Nama Belakang">
-          </div>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Nama Perguruan Tinggi</label>
-            <input type="text" class="form-control" placeholder="Nama Perguruan Tinggi">
-          </div>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Kode Perguruan Tinggi</label>
-            <input type="text" class="form-control" placeholder="Kode Perguruan Tinggi">
-          </div>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Kota</label>
-            <input type="text" class="form-control" placeholder="Kota">
-          </div>
-          <div class="form-group">
-            <label for="recipient-name" class="col-form-label">Nomer Telpon</label>
-            <input type="text" class="form-control" placeholder="Nomer Telpon">
-          </div>
-          <div class="form-group">
-            <label for="exampleFormControlSelect1">Jenis Kelamin</label>
-            <select class="form-control">
-            <option value="Laki-laki">Laki-laki</option>
-            <option value="Perempuan">Perempuan</option>
-            </select>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">NIM</label>
+              <input type="text" class="form-control" placeholder="NIM">
+            </div>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Nama Depan</label>
+              <input type="text" class="form-control" placeholder="Nama Depan">
+            </div>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Nama Belakang</label>
+              <input type="text" class="form-control" placeholder="Nama Belakang">
+            </div>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Nama Perguruan Tinggi</label>
+              <input type="text" class="form-control" placeholder="Nama Perguruan Tinggi">
+            </div>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Kode Perguruan Tinggi</label>
+              <input type="text" class="form-control" placeholder="Kode Perguruan Tinggi">
+            </div>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Kota</label>
+              <input type="text" class="form-control" placeholder="Kota">
+            </div>
+            <div class="form-group">
+              <label for="recipient-name" class="col-form-label">Nomer Telpon</label>
+              <input type="text" class="form-control" placeholder="Nomer Telpon">
+            </div>
+            <div class="form-group">
+              <label for="exampleFormControlSelect1">Jenis Kelamin</label>
+              <select class="form-control">
+                <option value="Laki-laki">Laki-laki</option>
+                <option value="Perempuan">Perempuan</option>
+              </select>
+            </div>
+          </form>
         </div>
-        </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Buat Akun</button>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Buat Akun</button>
+        </div>
       </div>
     </div>
   </div>
-</div>
   <!-- Vendor JS Files -->
   <script src="{{ asset('home/assets/vendor/aos/aos.js')}}"></script>
   <script src="{{ asset('home/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
@@ -499,8 +503,8 @@
   <script src="{{ asset('home/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
   <script src="{{ asset('home/assets/vendor/waypoints/noframework.waypoints.js')}}"></script>
   <script src="{{ asset('home/assets/vendor/php-email-form/validate.js')}}"></script>
-    <script src="{{asset('assets/js/jquery.min.js')}}"></script>
-    <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('assets/js/jquery.min.js')}}"></script>
+  <script src="{{asset('assets/js/bootstrap.bundle.min.js')}}"></script>
   <!-- Template Main JS File -->
   <script src="{{ asset('home/assets/js/main.js')}}"></script>
 
